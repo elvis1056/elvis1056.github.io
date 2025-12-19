9cd334fe1ef5299d5ffb24a6e4c6e29e2952a456
elvislin <john800116@hotmail.com>
2025-12-19 10:32:38 +0800

    feat: add Cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,10 @@
+import CartPageContent from '@/features/cart/CartPageContent';
+
+export const metadata = {
+  title: '購物車 | 5dpapa',
+  description: '查看您的購物車內容',
+};
+
+export default function CartPage() {
+  return <CartPageContent />;
+}
